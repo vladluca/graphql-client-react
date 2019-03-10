a9cef0fc4f00754d0349b3d926f4f6a177636383
Luca Vlad <luca.vlad08@gmail.com>
2019-03-10 13:48:31 +0200

    Update d.ts file.

--- graphql-client-react.d.ts
+++ graphql-client-react.d.ts
@@ -3,4 +3,4 @@ import { ComponentClass, ComponentType } from 'react'
 import { IGraphQLOptions } from './src/components/interfaces/IGraphQLOptions';
 import { IGraphQLInjectedProps } from './src/components/interfaces/IGraphQLInjectedProps';
 
-declare function graphql<TProps>(options: IGraphQLOptions): (WrappedComponent: ComponentType<TProps>) => ComponentClass<TProps & IGraphQLInjectedProps>;
+declare function graphql<TProps>(options?: IGraphQLOptions): (WrappedComponent: ComponentType<TProps>) => ComponentClass<TProps & IGraphQLInjectedProps>;
